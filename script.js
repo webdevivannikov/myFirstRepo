d463f07f22e9b9568be1f8be01768a9f4686b9e1
Alex Ivannikov <polkil0@yandex.ru>
2021-12-03 00:37:10 +0700

    создал новую ветку для lesson02

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-let title,
-    screens,
-    screenPrice,
+let title = 'Название проекта',
+    screens = "Простые, Сложные, Интерактивные",
+    screenPrice = 5000,
     rollback,
-    fullPrice,
-    adaptive;
+    fullPrice = 10000,
+    adaptive = true;
     
-alert ('Любой текст');
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+rollback = Math.trunc(getRandomArbitrary(1,100));
+console.log(typeof(title));
+console.log(typeof(fullPrice));
+console.log(typeof(adaptive));
+console.log(screens.length);
+console.log('Стоимость верстки экранов (' + screenPrice + ') рублей/ долларов/гривен/юани');
+console.log('Стоимость разработки сайта (' + fullPrice + ') рублей/ долларов/гривен/юани');
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback/100));
