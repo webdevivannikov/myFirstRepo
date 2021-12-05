135d2a08f3caf2df0ba122d7dfc39aa9ae1a9cb6
Alex Ivannikov <polkil0@yandex.ru>
2021-12-06 03:11:42 +0700

    создал новую ветку для lesson04

--- script.js
+++ script.js
@@ -1,49 +1,74 @@
 'use strict';
 
-let title = prompt('Как называется ваш проект?','Название проекта'),
+let rollback,
+    fullPrice,
+    servicePercentPrice,
+    allServicePrices,
+    title = prompt('Как называется ваш проект?','Название проекта'),
     screens = prompt("Какие типы экранов нужно разработать? (Перечислите через запятую)", 'Простые, сложные'),
     screenPrice = +prompt("Сколько будет стоить данная работа?", '5000'),
-    rollback,
-    fullPrice,
     adaptive = confirm('Нужен ли адаптив на сайте'),
     service1 = prompt('Какой дополнительный тип услуги нужен?', 'Услуга1'),
     servicePrice1 = +prompt('Сколько это будет стоить?', '1000'),
     service2 = prompt('Какой дополнительный тип услуги нужен?', 'Услуга2'),
-    servicePrice2 = +prompt('Сколько это будет стоить?', '1200'),
-    servicePercentPrice;
+    servicePrice2 = +prompt('Сколько это будет стоить?', '1200');
+
     
-if (screens != null)
-    screens.toLowerCase().split(", ");  
 function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-  }
+        return Math.random() * (max - min) + min;
+    }
+function getFullPrice(priceScreen, pricesService){
+        return priceScreen + pricesService;
+    }
+
+const getAllServicePrices = function(price1, price2){
+    return price1 + price2;
+};
+const getTitle = function(str){
+    if (str.trim() != '') {
+        return (str[0].toUpperCase() + str.slice(1)).trim();
+    }    
+};
+const getServicePercentPrices = function(total, percent){
+    return total - total* (percent/100);
+};
+const getArray = function(str){
+    if (str != null)
+        return str.toLowerCase().split(", ");
+};
+const showTypeOf = function(variable){
+    console.log(variable, typeof variable);
+};
+const getRollbackMessage = function(price) {
+    switch(true) {
+        case price >= 30000:
+            return 'Даем скидку 10%';
+        case price >= 15000 && price < 30000:
+            return 'Даем скидку 5';
+        case price < 15000 && price >= 0:
+            return 'Скидка не предусмотрена';
+        case price < 0:
+            return 'Что то пошло не так';
+    }
+};
+
+
+allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+fullPrice = Math.round(getFullPrice(screenPrice, allServicePrices));
+getTitle(title);
 rollback = Math.trunc(getRandomArbitrary(1,100));
-fullPrice = Math.round(screenPrice + servicePrice1 + servicePrice2);
-servicePercentPrice = fullPrice - fullPrice * (rollback/100);
-switch(true) {
-    case fullPrice >= 30000:
-        alert ('Даем скидку 10%');
-        break;
-    case fullPrice >= 15000 && fullPrice < 30000:
-        alert ('Даем скидку 5');
-        break;
-    case fullPrice < 15000 && fullPrice >= 0:
-        alert ('Скидка не предусмотрена');
-        break;
-    case fullPrice < 0:
-        alert ('Что то пошло не так');
-        break;
-}
+servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
 
 
 
-console.log(title);
-console.log(fullPrice);
-console.log(adaptive);
-if (screens != null)
-    console.log(screens.length);
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
 console.log('Стоимость верстки экранов (' + screenPrice + ') рублей/ долларов/гривен/юани');
 console.log('Стоимость разработки сайта (' + fullPrice + ') рублей/ долларов/гривен/юани');
 console.log('Какие типы экранов нужно разработать', screens);
-console.log(fullPrice * (rollback/100));
 console.log(servicePercentPrice);
+console.log(getArray(screens));
+console.log(getRollbackMessage(fullPrice));
+
